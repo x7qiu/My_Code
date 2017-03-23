89130d052157ac404681e017932567b9112c9e40
x7qiu <qiux0518@hotmail.com>
2017-03-22 19:01:52 -0700

    update comments

--- C_Notes/Strings.c
+++ C_Notes/Strings.c
@@ -3,17 +3,21 @@
 #include <string.h>
 
 
+// Notes on printf() specifiers:
+// 1) %s - treat it as string, print untill reaches a '\0'
+// 2) %p - treat it as address, print the address
+
+
 int  main(){
 // Several ways to "produce" a string
 
-    // 1)
+    // 1) Pointer to string literal
     char* str1 = "haha1";
-    printf("str1 is %s, with value str1 %p, with address &str1 %p\n", str1, (void*)str1, &str1);
+    printf("str1 is %s, point to %p, with address &str1 %p\n", str1, (void*)str1, &str1);
 
-    str1 = "haha2";
-    printf("str1 is %s, with value str1 %p, with address &str1 %p\n", str1, (void*)str1, &str1);
-    // st1 is pointer to some string literal.
-    // Different str1, same &str1.
+    str1 = "haha2"; // str1 now points to a different address
+    printf("str1 is %s, point to %p, with address &str1 %p\n", str1, (void*)str1, &str1);
+    
 /*
 The syntax char* str1 = "haha1" points a char pointer str1 to a read-only string literal.
 Making changes such as str[0] = 'H' is illegal.
@@ -22,35 +26,32 @@ With str1 = "haha2", str is pointing to another read-only string literal.
 So str1 now have a different value equaling the address of the new string literal, but the address of str1, aka. &str1, remains unchanged.
 */
     
-    // 2)
+    // 2) Character array
     char str2[] = "xixi1";
     str2[0] = 'X';
-    printf("str2 is %s, with value str2 %p, with address &str2 %p\n", str2, (void*)str2, &str2);
+    printf("str2 is %s, point to %p, with address &str2 %p\n", str2, (void*)str2, &str2);
 
-    strcpy(str2, "xixi2");
-    printf("str2 is %s, with value str2 %p, with address &str2 %p\n", str2, (void*)str2, &str2);
-    //Results in same str2 in %p and same &str2.
+    strcpy(str2, "xixi2");  // str2 still points to same address
+    printf("str2 is %s, point to %p, with address &str2 %p\n", str2, (void*)str2, &str2);
 /*
-The syntax char str2[] = "xixi1" INITIALIZE a char array, equiavlent to char str2[] = {'x', 'i', 'x', 'i', 0}. 
+The syntax char str2[] = "xixi1" INITIALIZE a char array, equiavlent to char str2[] = {'x', 'i', 'x', 'i', '\0'}. 
 Making changes to such a char array is legal. 
 However, once the char array is initialized, it cannot be reassigned by simply using "=". 
-This is because string is NOT a primitive type in C, and array doesn't support assignment, so that char array assignment with "=" is not supported. 
+This is because string is NOT a primitive type in C, and array doesn't support assignment.
 Instead, a library function such as strcpy, strncpy, or memcpy should be used. 
-Modifying a char array does not change the value of str2, which is the memory address of the first char in the array.
+Modifying a char array does not change the value of str2(in%p), which is the memory address of the first char in the array.
 */
-    // 3)
+    // 3) Dynamic allocation
     char* str3 = malloc(sizeof(char)* 10);
     strcpy(str3, "hehe");
     str3[0] = 'G';
-    printf("str3 is %s, with value str3 %p, with address &str3 %p\n", str3, (void*)str3, &str3);
+    printf("str3 is %s, point to %p, with address &str3 %p\n", str3, (void*)str3, &str3);
 
-    strcpy(str3, "GeGe");
-    printf("str3 is %s, with value str3 %p, with address &str3 %p\n", str3, (void*)str3, &str3);
+    strcpy(str3, "GeGe");   // str3 still points to same address
+    printf("str3 is %s, point to %p, with address &str3 %p\n", str3, (void*)str3, &str3);
     
     free(str3);
     return 0;
-    //Results in same str3 and &str3
-
 }
 
 //More Comments:
